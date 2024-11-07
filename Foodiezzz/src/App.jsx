c6637d6357f4ffa240dd0bc24a324bbcdb454947
Sparsh Singh <sparsh312003@gmail.com>
2024-11-07 19:25:22 +0530

    Added Dynamic search

--- Foodiezzz/src/App.jsx
+++ Foodiezzz/src/App.jsx
@@ -1,14 +1,10 @@
 import Header from "./components/Header";
 import Body from "./components/Body";
-
-const Footer = () => {};
-
 function App() {
   return (
     <div className="app">
       <Header />
       <Body />
-      <Footer />
     </div>
   );
 }
--- Foodiezzz/src/components/Body.jsx
+++ Foodiezzz/src/components/Body.jsx
@@ -3,25 +3,36 @@
 import { useState } from "react";
 import { restaurants, Restaurants_pic } from "../config";
 
+const FilterData = (Search, res) => {
+  const filterData = res.filter((e) =>
+    e.info.name.toLowerCase().includes(Search.toLowerCase())
+  );
+  return filterData;
+};
+
 const ResCard = ({ resData }) => {
   const { cloudinaryImageId, name, cuisines, avgRating, areaName, costForTwo } =
     resData?.info;
   return (
     <div className="res-card">
-      <img src={Restaurants_pic + cloudinaryImageId} />
+      <img src={Restaurants_pic + cloudinaryImageId} alt={name} />
       <h2 className="res-name">{name}</h2>
       <h3 className="res-cuisine">{cuisines.join(", ")}</h3>
       <h3 className="res-rating">{avgRating}</h3>
       <h3 className="res-location">{areaName}</h3>
-      <h3 className="res-location">{costForTwo}</h3>
+      <h3 className="res-cost">{costForTwo}</h3>
     </div>
   );
 };
 
 const Body = () => {
   const [Search, setSearch] = useState("");
+  const [res, setRes] = useState(restaurants);
+
   const handleInputChange = (e) => {
-    setSearch(e.target.value);
+    const input = e.target.value;
+    setSearch(input);
+    setRes(FilterData(input, restaurants));
   };
 
   return (
@@ -30,19 +41,31 @@ const Body = () => {
         <input
           className="search-txt"
           type="text"
-          placeholder="Search"
+          placeholder="Dynamic Search"
           value={Search}
           onChange={handleInputChange}
         />
-        <button className="search-btn">Search</button>
+        {/* <button
+          className="search-btn"
+          onClick={() => {
+            const data = FilterData(Search, res);
+            setRes(data); // Use restaurants array for filtering
+          }}
+        >
+          Search
+        </button> */}
       </div>
 
       <div className="res-container">
-        {restaurants.map((restaurants) => (
-          <ResCard resData={restaurants} key={restaurants.info.id} />
-        ))}
+        {res.length > 0 ? (
+          //manually=>     <ResCard resData={res[0]} />
+          res.map((res) => <ResCard resData={res} key={res.info.id} />)
+        ) : (
+          <p>No results found</p> // Display a message if no results match
+        )}
       </div>
     </div>
   );
 };
+
 export default Body;
